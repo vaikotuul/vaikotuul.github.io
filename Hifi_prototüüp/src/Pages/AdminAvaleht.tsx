debf8e271e2f6655092893fb24312d4d11c80f96
ArminJaemaa <ajaemaa@tlu.ee>
2025-05-07 16:54:06 +0300

    lisatud Login, admin 2 lehte

---
+++ Hifi_prototüüp/src/Pages/AdminAvaleht.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function AdminAvaleht() {
+  return (
+    <div>
+        <div className='buttonContainer'>
+        <Link to={'/admin/seadmete-juhtimine'}>
+            <button>Seadete juhtimin</button>
+        </Link>
+        <button>Ajapõhine automatiseerimine</button>
+        <button>Kasutajaliidese redigeerimine</button>
+        </div>
+    </div>
+  )
+}
