053d4dc4930dc4322ad8750d47227fa92b6972c6
Mattias Tamm <182494580+tammmatTLU@users.noreply.github.com>
2025-05-07 15:10:52 +0000

    Muudetud kujundust. Lisatud sisse logimise lehega navigeerimine.

--- Hifi_prototüüp/src/Pages/AdminAvaleht.tsx
+++ Hifi_prototüüp/src/Pages/AdminAvaleht.tsx
@@ -3,18 +3,23 @@ import { Link } from 'react-router-dom'
 
 export default function AdminAvaleht() {
   return (
-    <div>
-        <div className='buttonContainer'>
-        <Link to={'/admin/seadmete-juhtimine'}>
-            <button>Seadete juhtimine</button>
+    <>
+      <header>
+        <Link to={"/"}>
+        <button id="logOut">Logi välja</button>
         </Link>
-        <Link to={'/admin/automatiseerimine'}>
-          <button>Ajapõhine automatiseerimine</button>
-        </Link>
-        <Link to={'/admin/redigeerimine'}>
-          <button>Kasutajaliidese redigeerimine</button>
-        </Link>
-        </div>
-    </div>
+      </header>
+      <div className='buttonContainer'>
+      <Link to={'/admin/seadmete-juhtimine'}>
+        <button>Seadete juhtimine</button>
+      </Link>
+      <Link to={'/admin/automatiseerimine'}>
+        <button>Ajapõhine automatiseerimine</button>
+      </Link>
+      <Link to={'/admin/redigeerimine'}>
+        <button>Kasutajaliidese redigeerimine</button>
+      </Link>
+      </div>
+    </>
   )
 }
