fae7fcd9e4b99d17452a88b474b51f994cdeea00
Mattias Tamm <182494580+tammmatTLU@users.noreply.github.com>
2025-05-08 14:59:34 +0000

    Algeline control panel kujundus loodud A-001 auditooriumile.

---
+++ katse_1/src/components/Kujundus.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import Tagasi from '../components/Tagasi'
+import '../Kujundus.css'
+
+export function Kujundus_A001(){
+    
+
+    return(
+        <>
+        <div id="controlPanel">
+            <div id="lightsControl">
+                <span className='lights-indicator'></span>
+                <button id="lightsOn">Tuled Sisse</button>
+                <button id="lightsOff">Tuled Välja</button>
+            </div>
+            <div id="screenControl">
+                <span className="screen-indicator"></span>
+                <button id="screenOn">Ekraan Sisse</button>
+                <button id="screenOff">Ekraan Välja</button>
+            </div>
+            <div id="projectorControl">
+                <span className="projector-indicator"></span>
+                <button id="projectorOn">Projektor Sisse</button>
+                <button id="projectorOff">Projektor Välja</button>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export function Kujundus_A002(){
+
+
+    return(
+        <>
+        <Tagasi/>
+        </>
+    )
+}
+
+export function Kujundus_A003(){
+
+
+    return(
+        <>
+        <Tagasi/>
+        </>
+    )
+}
